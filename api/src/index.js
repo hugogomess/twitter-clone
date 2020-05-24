29ff786912594271a4f1fca4ef8819467b97173a
hugogomess <hugogomes02@gmail.com>
2020-05-24 19:13:56 -0300

    add mongodb connection

--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,12 @@
 require('dotenv/config');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3333;
+const mongoUrl = process.env.MONGO_URL;
 const app = express();
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
 app.get('/', (req, res) => {
   return res.send('Hello World');
