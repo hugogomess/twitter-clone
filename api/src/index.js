e512221423924cdbd8bd1b43ecc75e80fc9ec862
hugogomess <hugogomes02@gmail.com>
2020-05-24 19:11:32 -0300

    add dotenv config

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
+require('dotenv/config');
 const express = require('express');
-const port = process.env.PORT || 3333;
 
+const port = process.env.PORT || 3333;
 const app = express();
 
 app.get('/', (req, res) => {
