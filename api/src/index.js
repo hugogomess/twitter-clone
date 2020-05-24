2e539af63e5955b92506b773931813ab6953abb4
hugogomess <hugogomes02@gmail.com>
2020-05-24 18:54:04 -0300

    update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const port = process.env.PORT || 3333;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
