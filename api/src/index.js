6ea7508f1d0147f9e0d3a4759d0e8572cd50119b
hugogomess <hugogomes02@gmail.com>
2020-05-24 23:17:29 -0300

    add cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv/config');
 require('./database');
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const port = process.env.PORT || 3333;
@@ -13,8 +14,9 @@ app.use((req, res, next) => {
   return next();
 });
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
