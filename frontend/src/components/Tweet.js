3e312dae15e45afb2111f004863c1be26f02e107
hugogomess <hugogomes02@gmail.com>
2020-05-25 02:52:29 -0300

    finish like feature

--- frontend/src/components/Tweet.js
+++ frontend/src/components/Tweet.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
 
+import api from '../services/api';
 import './Tweet.css';
 import like from '../like.svg';
 
 export default class Tweet extends Component {
+  handleLike = async () => {
+    const { _id } = this.props.tweet;
+
+    await api.post(`/tweets/like/${_id}`);
+  }
+  
   render() {
     const { tweet } = this.props;
 
@@ -11,7 +18,7 @@ export default class Tweet extends Component {
       <li className="tweet">
         <b>{tweet.author}</b>
         <p>{tweet.content}</p>
-        <button type="button">
+        <button type="button" onClick={this.handleLike}>
           <img src={like} alt="like"/> {tweet.likes}
         </button>
       </li>
