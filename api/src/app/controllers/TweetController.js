16bf9e9b964dd1b2264ec02e488ff3a456ac1061
hugogomess <hugogomes02@gmail.com>
2020-05-24 22:37:00 -0300

    add tweet controller

---
+++ api/src/app/controllers/TweetController.js
@@ -0,0 +1,13 @@
+const Tweet = require('../models/Tweet');
+
+class TweetController {
+  async index(req, res) {
+
+  }
+
+  async store(req, res) {
+
+  }
+}
+
+module.exports = new TweetController();
--- api/src/routes.js
+++ api/src/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const TweetController = require('./app/controllers/TweetController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.send('tresete');
-});
+routes.get('/tweets', TweetController.index);
+routes.post('/tweets', TweetController.store);
 
 module.exports = routes;
