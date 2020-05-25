bebfbbd769f5da847b2038d5b00e20fbf384dbce
hugogomess <hugogomes02@gmail.com>
2020-05-24 22:41:07 -0300

    update tweet controller

--- api/src/app/controllers/TweetController.js
+++ api/src/app/controllers/TweetController.js
@@ -2,11 +2,15 @@ const Tweet = require('../models/Tweet');
 
 class TweetController {
   async index(req, res) {
+    const tweets = await Tweet.find().sort('-createdAt');
 
+    return res.json(tweets);
   }
 
   async store(req, res) {
+    const tweet = await Tweet.create(req.body);
 
+    return res.json(tweet);
   }
 }
 
